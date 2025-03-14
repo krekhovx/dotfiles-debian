ba5e64645f5867675d1504cd2702b8ca10ab28f8
Kirill Rekhov <krekhov.dev@gmail.com>
2025-03-14 16:58:58 +0300

    change execution order of deb-clean.sh

--- .local/bin/deb-clean.sh
+++ .local/bin/deb-clean.sh
@@ -4,6 +4,7 @@
 # plus debs autoremove and autoclean.
 #
 
-dpkg -l | awk '/^rc/{print $2}' | xargs sudo apt-get purge -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean
+
+dpkg -l | awk '/^rc/{print $2}' | xargs sudo apt-get purge -y
