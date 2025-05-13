cb2a57f2c779016ba8d0cb54eaa42f516b4a16f1
Kirill Rekhov <krekhov.dev@gmail.com>
2025-05-13 20:09:02 +0300

    .local/bin/pn.sh: update script

--- .local/bin/pn.sh
+++ .local/bin/pn.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #
-# Push to notes:
-# https://github.com/krekhovx/notes
+# Push to notes: https://github.com/krekhovx/notes
 # I'm not concerned about commit descriptions in this project.
 #
 
 gitstatus=$(git status -s)
-tree > README
+tree > ~/git/myenv/notes/README
 git add .
 git commit -m "$gitstatus"
 git push
