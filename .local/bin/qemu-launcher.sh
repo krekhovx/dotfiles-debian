8b3121e0baf6710f933efb472d5c81ccbf080c3c
Kirill Rekhov <krekhov.dev@gmail.com>
2025-07-02 23:07:27 +0300

    qemu-launcher.sh: increase qcow2 size to 50G

--- .local/bin/qemu-launcher.sh
+++ .local/bin/qemu-launcher.sh
@@ -34,7 +34,7 @@ case "$1" in
 '-qch'|'--qemu-cdrom-hda')
 	[ -z "$2" ] && usage 1
 	f=$(basename "$(mktemp -u)")
-	qemu-img create -f qcow2 "/tmp/$f.img" 30G
+	qemu-img create -f qcow2 "/tmp/$f.img" 50G
 	qemu-system-x86_64 -boot d -cdrom "$2" -m 2048 -hda "/tmp/$f.img" \
 	-net nic -net user -enable-kvm -cpu host -smp "$(nproc)"
 	;;
