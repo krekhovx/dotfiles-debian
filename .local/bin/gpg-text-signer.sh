d98d9eb8b5412213af35567cbde65d4633622181
Kirill Rekhov <krekhov.dev@gmail.com>
2025-07-09 18:03:25 +0300

    gpg-text-signer.sh: remove signature + minor improvements

--- .local/bin/gpg-text-signer.sh
+++ .local/bin/gpg-text-signer.sh
@@ -24,35 +24,20 @@ exit 0
 fi
 }
 
-message_template()
-{
-text=$(mktemp)
-cat << EOF > $text
-
-
----
-Regards, Kirill Rekhov
-
-GPG Fingerprint:
-2640 769D FDA1 AAA0 F863  D1AE 5F2C 5905 519C E0A0
-EOF
-vim $text
-}
-
 case "$1" in
 '-c'|'--clearsign')
-	message_template
+	text=$(mktemp) && vim $text
 	gpg --clearsign $text
 	shred -uzn 8 $text
-	cat $text.asc | xsel -b -i
+	xsel -b -i < $text
 	shred -uzn 8 $text.asc
 	echo "> paste signed text from clipboard"
 	;;
 
 '-d'|'--detachsign')
-	message_template
+	text=$(mktemp) && vim $text
 	gpg --detach-sign --armor -o ~/signature.asc $text
-	cat $text | xsel -b -i
+	xsel -b -i < $text
 	shred -uzn 8 $text
 	echo "> paste signed text from clipboard"
 	echo "> attach signature.asc"
