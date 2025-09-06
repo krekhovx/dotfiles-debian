b2d3aa56bda55c0efff8ed78d92bf2429830ab15
Kirill Rekhov <krekhov.dev@gmail.com>
2025-09-06 23:57:57 +0300

    .local/bin/multi-git.sh: remove stupid options

--- .local/bin/multi-git.sh
+++ .local/bin/multi-git.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Execute Git commands like 'clone', 'pull', or 'status' across multiple Git
+# Execute Git commands like 'pull', 'status' across multiple Git
 # projects for easy management.
 #
 
@@ -15,8 +15,6 @@ $(echo -e "\e[96mUsage: $(basename $0) [options]\e[0m")
 Multiple Git Control (simple git operations).
 
   [option]
-  -cs, --clone-salsa                        clone my favorite Salsa projects in current directory
-  -ck, --clone-krekhovx                     clone my GitHub projects in current directory
   -p, --pull </path/to/git/projects>        pull from the specified Git projects, all branches
   -s, --status </path/to/git/projects>      status from the specified Git projects, current branch
   -h, --help                                show this help and exit
@@ -83,60 +81,7 @@ git_command()
 	done
 }
 
-clone_salsa()
-{
-	dir=$1
-	list=($2)
-
-	mkdir $dir 2>/dev/null
-
-	for name in ${list[*]}; do
-		vcs=$(apt-get source --print-uris "$name" 2>/dev/null | grep -E 'https|salsa' | head -1)
-		if [ -z "$vcs" ]; then
-			echo >&2 "$name is not loaded on salsa."
-			echo; continue
-		fi
-
-		sdir=$(basename 2>/dev/null $(basename "$vcs" .git))
-		if [ ! -d "$dir/$sdir" ]; then
-			git clone "$vcs" "$dir/$sdir"
-			echo
-		fi
-	done
-}
-
 case "$1" in
-	# clone my GitHub projects
-	'-ck'|'--clone-krekhovx')
-		projects=(krekhovx krxnotes dotfiles-debian
-		pbuilder-amd64-helper quake3-terminal-theme)
-
-		for name in ${projects[*]}; do
-			if [ ! -d "$name" ]; then
-				git clone "git@github.com:krekhovx/$name"
-				echo
-			fi
-		done
-	;;
-
-	# clone my favorite Salsa projects
-	'-cs'|'--clone-salsa')
-		# packages I monitor
-		watch_packages=(xfce4-terminal xterm mc tmux vim \
-		how-can-i-help popularity-contest telegram-desktop \
-		systemd strace eatmydata)
-
-		xfce_packages=$(grep-aptavail -F Maintainer -s Package -n \
-		"Debian Xfce Maintainers <debian-xfce@lists.debian.org>")
-
-		vim_packages=$(grep-aptavail -F Maintainer -s Package -n \
-		"Debian Vim Maintainers <team+vim@tracker.debian.org>")
-
-		clone_salsa "watch" "${watch_packages[*]}"
-		clone_salsa "xfce" "${xfce_packages[*]}"
-		clone_salsa "vim" "${vim_packages[*]}"
-	;;
-
 	'-p'|'--pull')
 		[ -z "$2" ] && usage 1
 		git_command "pull" "$2"
